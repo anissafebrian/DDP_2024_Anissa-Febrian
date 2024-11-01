98e1c681cab42e5b264a35a523b10dc5a25d1e3e
anissafebrian <anissafebrian51@gmail.com>
2024-11-01 20:30:07 +0700

    tugas5_Anissa Febrian

---
+++ ddp.py
@@ -0,0 +1,11 @@
+Kendaraan = ['beat street', 'motor', 200, 'hitam', 2]
+print(Kendaraan)
+                 
+Kendaraan.append('16jt')
+print(Kendaraan)
+
+Kendaraan.append('matic')
+print(Kendaraan)
+                 
+Kendaraan.insert(2, 'honda')
+print(Kendaraan)
