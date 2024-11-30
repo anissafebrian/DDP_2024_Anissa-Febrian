af085fc630ab3f659f7402ae8d4489eab751a21c
anissafebrian <anissafebrian51@gmail.com>
2024-11-30 20:33:42 +0700

    Add files via upload

--- ddp_10/aplikasi/bangunruang.py
+++ ddp_10/aplikasi/bangunruang.py
@@ -14,10 +14,12 @@ def l_tabung(phi, jari_jari, tinggi):
     hitung = 2 * 3.14 * (jari_jari + tinggi)
     print(f'Luas tabung adalah {hitung}')
 
-def l_limas(alas, sisi):
-    hitung = alas + sisi
+def l_limas_segitiga(tinggi_alas, alas, tinggi1, tinggi2, tinggi3):
+    luas_alas = 1/2 *  tinggi_alas * alas 
+    total_l_tegak = 1/2 * alas * tinggi1 + 1/2 * alas * tinggi2 + 1/2 * alas * tinggi3
+    hitung = luas_alas + total_l_tegak 
     print(f'Luas limas adalah {hitung}')
 
-def l_prisma(alas, sisi):
-    hitung = ( 2 * alas ) + sisi
+def l_prisma_segitiga(alas, tinggi_segitiga, tinggi_prisma,  p1, p2, p3):
+    hitung = alas * tinggi_segitiga + ((p1 + p2 + p3) * tinggi_prisma)
     print(f'Luas prisma adalah {hitung}')
--- ddp_10/aplikasi/call.py
+++ ddp_10/aplikasi/call.py
@@ -10,5 +10,11 @@ import bangunruang
 bangunruang.l_balok(3, 5, 7)
 bangunruang.l_kubus(7)
 bangunruang.l_tabung(4, 5, 3)
-bangunruang.l_limas(15, 10)
-bangunruang.l_prisma(34, 6)
+bangunruang.l_limas_segitiga(4, 6, 5, 7, 6)
+bangunruang.l_prisma_segitiga(6, 4, 10, 6, 5, 7)
+
+import hitung
+hitung.bagi(10,2)
+hitung.kali(2,9)
+hitung.kurang(20,7)
+hitung.tambah(20,6)
